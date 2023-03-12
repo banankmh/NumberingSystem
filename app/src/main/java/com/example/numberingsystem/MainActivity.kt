29f5ff5aabc2361a80267cce0b907deef0418f64
Banan Kamal <bananhmdan962@gmail.com>
2023-03-13 00:50:53 +0200

    init commit

---
+++ app/src/main/java/com/example/numberingsystem/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.numberingsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var buttonClean: Button
+    private lateinit var buttonConvert: Button
+    private lateinit var buttonToDecimal: Button
+    private lateinit var buttonToBinary: Button
+    private lateinit var buttonToOctal: Button
+    private lateinit var buttonToHexaDecimal: Button
+    private lateinit var enterdeNumber: EditText
+    private lateinit var result: TextView
+    private var number = 0.0
+    private var currentNumberSystem: NumbringSystem? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initView()
+        addCallbacks()
+    }
+
+    private fun addCallbacks() {
+        buttonClean.setOnClickListener {
+            clearInput()
+        }
+        buttonToBinary.setOnClickListener {
+
+            prepareOperation(NumbringSystem.ToBinary)
+        }
+        buttonToDecimal.setOnClickListener {
+            prepareOperation(NumbringSystem.ToDecimal)
+        }
+        buttonToOctal.setOnClickListener {
+            prepareOperation(NumbringSystem.ToOctal)
+        }
+        buttonToHexaDecimal.setOnClickListener {
+            prepareOperation(NumbringSystem.ToHexa)
+        }
+        buttonConvert.setOnClickListener {
+            doCurrentConversion()
+
+        }
+    }
+
+
+
+    private fun doCurrentConversion() {
+        val inputNumber = enterdeNumber.text.toString()
+        if (inputNumber.isNotEmpty()) {
+
+                number = inputNumber.toDouble()
+                when (currentNumberSystem) {
+                    NumbringSystem.ToBinary -> result.text = number.toLong().toString(2)
+                    NumbringSystem.ToDecimal -> result.text = number.toString()
+                    NumbringSystem.ToOctal -> result.text = number.toLong().toString(8)
+                    NumbringSystem.ToHexa -> result.text = number.toLong().toString(16)
+                    null -> result.text = "Please select a conversion option"
+                }
+
+        } else {
+            result.text = "Please enter a number to convert"
+        }
+    }
+
+    private fun prepareOperation(numberSystem: NumbringSystem) {
+        number=enterdeNumber.text.toString().toDouble()
+        currentNumberSystem = numberSystem
+
+    }
+
+    private fun clearInput() {
+        enterdeNumber.text = null
+        result.text = ""
+    }
+
+    private fun initView() {
+        buttonClean = findViewById(R.id.btn_clean)
+        buttonConvert = findViewById(R.id.btn_Convert)
+        buttonToDecimal = findViewById(R.id.btn_toDecimal)
+        buttonToBinary = findViewById(R.id.btn_toBinary)
+        buttonToOctal = findViewById(R.id.btn_toOctal)
+        buttonToHexaDecimal = findViewById(R.id.btn_toHexaDecimal)
+        enterdeNumber = findViewById(R.id.EntredNumber)
+        result = findViewById(R.id.result)
+    }
+}
---
+++ app/src/main/java/com/example/numberingsystem/NumbringSystem.kt
@@ -0,0 +1,8 @@
+package com.example.numberingsystem
+
+enum class NumbringSystem {
+    ToBinary,
+    ToDecimal,
+    ToOctal,
+    ToHexa
+}
